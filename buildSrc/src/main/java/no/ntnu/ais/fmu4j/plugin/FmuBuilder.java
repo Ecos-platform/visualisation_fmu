b1e28f4ccfaea0dbef1c1b3d73c50035b54a944c
Lars Ivar Hatledal <laht@ntnu.no>
2022-04-29 08:19:43 +0200

    Update

--- buildSrc/src/main/java/no/ntnu/ais/fmu4j/plugin/FmuBuilder.java
+++ buildSrc/src/main/java/no/ntnu/ais/fmu4j/plugin/FmuBuilder.java
@@ -10,8 +10,6 @@ import java.util.List;
 
 public class FmuBuilder extends DefaultTask {
 
-    private static final String BUILDER_VERSION = "0.1.0";
-
     @Input
     private List<String> mainClasses = new ArrayList<>();
 
@@ -26,7 +24,6 @@ public class FmuBuilder extends DefaultTask {
     @TaskAction
     void fmu4j() {
         for (String mainClass : mainClasses) {
-
             String archiveBaseName = getProject().getName();
             File jar = new File(getProject().getBuildDir(), "libs/" + archiveBaseName + ".jar").getAbsoluteFile();
             no.ntnu.ais.fmu4j.FmuBuilder builder = new no.ntnu.ais.fmu4j.FmuBuilder(mainClass, jar, null);
