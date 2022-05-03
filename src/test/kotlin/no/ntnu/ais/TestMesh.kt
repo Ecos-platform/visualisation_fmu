c8adce211915a9d7ccabb6f736d72b740b1ccf2a
Lars Ivar Hatledal <laht@ntnu.no>
2022-05-03 08:24:53 +0200

    Update test

--- src/test/kotlin/no/ntnu/ais/TestMesh.kt
+++ src/test/kotlin/no/ntnu/ais/TestMesh.kt
@@ -1,27 +1,45 @@
 package no.ntnu.ais
 
 import java.util.Scanner
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.concurrent.thread
 
 object TestMesh {
 
     @JvmStatic
     fun main(args: Array<String>) {
 
-        val fmu = VisualisationFmu(mapOf(
-            "instanceName" to "instance",
-            "resourceLocation" to "data/TestMeshConfig"
-        ))
+        val fmu = VisualisationFmu(
+            mapOf(
+                "instanceName" to "instance",
+                "resourceLocation" to "data/TestMeshConfig"
+            )
+        )
         fmu.__define__()
         fmu.enterInitialisationMode()
-
         fmu.exitInitialisationMode()
 
-        val sc = Scanner(System.`in`)
-        while (sc.hasNext()) {
-            if (sc.next() == "q")
-                break
+        val stop = AtomicBoolean(false)
+        thread {
+            val sc = Scanner(System.`in`)
+            while (sc.hasNext()) {
+                if (sc.next() == "q") {
+                    stop.set(true)
+                    break
+                }
+            }
         }
 
+        var t = 0.0
+        val dt = 0.1
+        var angle = 0.0
+        while (!stop.get()) {
+            fmu.doStep(t, dt)
+            fmu.setReal(longArrayOf(fmu.getValueRef("transform[0].rotation.y")), doubleArrayOf(angle))
+            angle += 1
+            t += dt
+            Thread.sleep(10)
+        }
         fmu.terminate()
 
     }
